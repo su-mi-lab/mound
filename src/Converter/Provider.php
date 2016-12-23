59d62e2d030016b4bb8a4bc58ab6f717237b08a9
sumiyoshi <mail.sumimaru@gmail.com>
2016-12-23 20:40:14 +0900

    Add Provider method

--- src/Converter/Provider.php
+++ src/Converter/Provider.php
@@ -11,4 +11,24 @@ use Mound\ProviderInterface;
 class Provider implements ProviderInterface
 {
 
+    /**
+     * @param string $name
+     * @param array $keys
+     * @param array $options
+     * @return Provider
+     */
+    public function attach(string $name, array $keys, array $options = []): Provider
+    {
+        return $this;
+    }
+
+    /**
+     * @param array $data
+     * @param array $options
+     * @return Provider
+     */
+    public function exec(array $data, array $options = []): Provider
+    {
+        return $this;
+    }
 }
--- src/ProviderInterface.php
+++ src/ProviderInterface.php
@@ -8,5 +8,18 @@ namespace Mound;
  */
 interface ProviderInterface
 {
+    /**
+     * @param string $name
+     * @param array $keys
+     * @param array $options
+     * @return ProviderInterface
+     */
+    public function attach(string $name, array $keys, array $options = []): static;
 
+    /**
+     * @param array $data
+     * @param array $options
+     * @return ProviderInterface
+     */
+    public function exec(array $data, array $options = []): static;
 }
